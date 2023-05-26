e65b0656d9213261bd2d4f91d65bf8e23382c4bf
Johnny <johnnyscanlon22@gmail.com>
2023-05-26 16:02:41 +0100

    Further test that everything is pushing ok

--- scripts/main.js
+++ scripts/main.js
@@ -27,7 +27,7 @@ if (!localStorage.getItem("name")) {
     setUserName();
 } else {
     const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+    myHeading.textContent = `Test Site by: , ${storedName}`;
 };
 
 myButton.onclick = () => {
